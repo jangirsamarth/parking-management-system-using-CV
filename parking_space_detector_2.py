e236848a20fc525c67407c68b174090e93cbe653
Yash Balyan <72466067+yashbalyan08@users.noreply.github.com>
2022-10-30 19:45:45 +0530

    Add files via upload

---
+++ parking_space_detector_2.py
@@ -0,0 +1,34 @@
+import pickle
+import cv2
+
+# SIZE OF BOX IN WHICH CAR IS
+width, height = 12, 60
+
+try:
+    with open('CarParkPos4', 'rb') as f:
+        posList = pickle.load(f)
+except:
+    posList = []
+
+
+def mouseClick(events, x, y, flags, params):
+    if events == cv2.EVENT_LBUTTONDOWN:
+        posList.append((x, y))
+    if events == cv2.EVENT_RBUTTONDOWN:
+        for i, pos in enumerate(posList):
+            x1, y1 = pos
+            if x1 < x < x1 + width and y1 < y < y1 + height:
+                posList.pop(i)
+
+    with open('CarParkPos4', 'wb') as f:
+        pickle.dump(posList, f)
+
+
+while True:
+    img = cv2.imread('car_parklot_2.jpeg')
+    for pos in posList:
+        cv2.rectangle(img, pos, (pos[0] + width, pos[1] + height), (255, 0, 255), 2)
+
+    cv2.imshow("Image", img)
+    cv2.setMouseCallback("Image", mouseClick)
+    cv2.waitKey(1)
